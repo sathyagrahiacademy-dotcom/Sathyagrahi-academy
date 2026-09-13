e31ab362f0637086765ab332085c646d4342a632
sathyagrahiacademy-dotcom <sathyagrahiacademy@gmail.com>
2026-09-13 08:43:39 +0530

    feat: load secure exam credential controller

--- admin-examinations-nav.js
+++ admin-examinations-nav.js
@@ -37,7 +37,8 @@
     const masterPhase1Enabled=window.SGA_EXAMINATIONS_MASTER_PHASE1_ENABLED===true;
     if(masterPhase1Enabled){
       loadScript('examControlCenterUi','exam-control-center-ui.js?v=20260908-1',()=>{
-        loadScript('adminExamControlCenter','admin-exam-control-center.js?v=20260910-5',()=>{
+        loadScript('adminExamControlCenter','admin-exam-control-center.js?v=20260913-1',()=>{
+          loadScript('adminExamCredentialsUi','admin-exam-credentials-ui.js?v=20260913-1');
           loadScript('adminExamWorkspaceRoute','admin-exam-workspace-route.js?v=20260910-2');
         });
       });
